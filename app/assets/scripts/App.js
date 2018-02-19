fd54a31164fe6073676140c8378e3516bd7aed8f
Dusan <koturovicdusan@gmail.com>
2018-02-19 20:43:23 +0100

    Successfully implemented lightbox.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,6 +1,8 @@
-import $ from 'jquery';
+import $ from 'jquery'
+import Lightbox2 from "../../../node_modules/lightbox2/dist/js/lightbox";
 import MobileMenu from './modules/MobileMenu';
 import StickyHeader from './modules/StickyHeader';
 
 var mobileMenu = new MobileMenu();
 var stickyHeader = new StickyHeader();
+var lightbox = new Lightbox2();
